e58bd4617aee7d1874195967ff4be2480447cb9a
tigerBreedlove <54365858+tigerBreedlove@users.noreply.github.com>
2019-10-02 13:54:51 -0400

    Update edit.php

--- webapp/edit.php
+++ webapp/edit.php
@@ -23,34 +23,31 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	
 	
 	
-	if (!$dbconnecterror) {
-		try {
-			//make a call to the api
-			
-			//build url for api
-			$url = "http://3.213.65.50/api/task.php?listID=$listID";
-			
-			//create json string
-			$data = array('completed'=>$complete,'taskName'=>$listItem,'taskDate'=>$finBy);
-			$data_json = json_encode($data); //converts array to string
-			
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-			curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			$response  = curl_exec($ch); //body of the response
-			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-			curl_close($ch);
-			
-			//status code 204
-			if($httpcode==203) {
-				header("Location: index.php");
-			} else {
-				header("Location: index.php?error=edit");
-			}
-		}
+
+	//make a call to the api
+	
+	//build url for api
+	$url = "http://3.213.65.50/api/task.php?listID=$listID";
+	
+	//create json string
+	$data = array('completed'=>$complete,'taskName'=>$listItem,'taskDate'=>$finBy);
+	$data_json = json_encode($data); //converts array to string
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response  = curl_exec($ch); //body of the response
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	
+	//status code 204
+	if($httpcode==203) {
+		header("Location: index.php");
+	} else {
+		header("Location: index.php?error=edit");
 	}
 }
 ?>
